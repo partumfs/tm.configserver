978ecec50319538a447b33a6ab4d987d549439b4
David Monteiro <david25616@hotmail.com>
2023-03-04 20:21:02 +0000

    first commit

---
+++ src/test/java/com/partumsf/tmconfigservices/TmconfigservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partumsf.tmconfigservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmconfigservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
